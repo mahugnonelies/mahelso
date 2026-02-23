09653127b1ad6912643036dc5d702c11e77fd419
mahugnonelies <227068391+mahugnonelies@users.noreply.github.com>
2026-02-23 11:03:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const cards = document.querySelectorAll(".app-card");
 
+window.addEventListener("scroll", () => {
+  cards.forEach(card => {
+    const position = card.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    if(position < screenHeight - 100){
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+    }
+  });
+});
